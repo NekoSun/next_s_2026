0e4cff5029e1dd40a458f397b7755f2587d891f2
NekoSun <webneko@nekoweb.ru>
2026-02-12 16:41:13 +0300

    + TypeOrm, TypeOrmConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTypeOrmConfig } from './config/typeorm.config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory: getTypeOrmConfig,
+      inject: [ConfigService]
+    }), TaskModule, MovieModule],
   controllers: [AppController],
   providers: [AppService],
-  imports: [TaskModule, MovieModule],
 })
-export class AppModule {}
+export class AppModule { }

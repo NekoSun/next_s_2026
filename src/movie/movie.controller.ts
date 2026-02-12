4f009fec0c264b287de271179ad8bd738ab66a84
NekoSun <webneko@nekoweb.ru>
2026-02-12 15:13:12 +0300

    декораторы запросов

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,7 +1,59 @@
-import { Controller } from '@nestjs/common';
-import { MovieService } from './movie.service';
+import { Body, Controller, Get, Headers, Param, Post, Query, Req, Res } from '@nestjs/common';
+import type { Request, Response } from 'express';
 
-@Controller('movie')
+@Controller('movies')
 export class MovieController {
-  constructor(private readonly movieService: MovieService) {}
+  @Get()
+  findAll(@Query('genre') genre: string) {
+    return genre 
+    ? `Фильмы в жанре: ${genre}`
+    : [
+      {
+        title: 'Fine Club'
+      },
+      {
+        title: 'Pulp Fiction'
+      }
+    ]
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return {id}
+  }
+
+  @Post()
+  create(@Body() body: {title: string, genr: string}) {
+    // return `Фильм "${title}" был добавлен`
+    return body
+  }
+
+  @Get('headers')
+  getHeaders(@Headers() headers: any) {
+    return headers
+
+  }
+
+  @Get('user-agent')
+  getUserAgent(@Headers('user-agent') userAgent: string) {
+    return {userAgent}
+
+  }
+
+  @Get('request')
+  getRequestDetails(@Req() req: Request) {
+    return {
+      method: req.method,
+      url: req.url,
+      headers: req.headers,
+      query: req.query,
+      params: req.params
+    }
+  }
+
+
+  @Get('response')
+  getResponseDetails(@Res() res: Response) {
+    res.status(201).json({message: 'Прывет Мыр!'})
+  }
 }

5b20f08d450102b3aa5a6b61fc5243e27fc72c18
NekoSun <webneko@nekoweb.ru>
2026-02-12 13:26:38 +0300

    + movie

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [TaskModule],
+  imports: [TaskModule, MovieModule],
 })
 export class AppModule {}
